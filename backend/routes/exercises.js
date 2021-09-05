6e16fd5be4b28a50ac38bd50b14521aa63dca2ae
Rupangkan <every1isnotrupangkan@gmail.com>
2021-09-05 10:57:34 +0530

    Finished backend

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -26,4 +26,31 @@ router.route('/add').post((req, res) => {
 
 })
 
+router.route('/:id').get((req, res) => {
+    exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json('Error: ' + err))
+})
+
+router.route('/:id').delete((req, res) => {
+    exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Exercise Deleted!'))
+    .catch(err => res.status(400).json('Error: ' + err))
+})
+
+router.route('/update/:id').post((req, res) => {
+    exercise.findByIdAndUpdate(req.params.id)
+    .then(exercise => {
+        exercise.username = req.body.username
+        exercise.description = req.body.description
+        exercise.duration = Number(req.body.duration)
+        exercise.date = Date.parse(req.body.date)
+
+        exercise.save()
+        .then(() => res.json('Exercise updated successfully!'))
+        .catch(err => res.status(400).json('Error: ' + err))
+    })
+    .catch(err => res.status(400).json('Error: ' + err))
+})
+
 module.exports = router
